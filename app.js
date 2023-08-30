0333fbcf6a15236998c6a6a7c17ec5ef9e296ac5
adithyan pr <108983785+Aathi-pr@users.noreply.github.com>
2023-08-30 16:54:10 +0530

    Add files via upload
    fixed some bugs and added footer

---
+++ app.js
@@ -0,0 +1,33 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.navigation');
+        const navLinks = document.querySelectorAll('.navigation li');
+    
+        burger.addEventListener('click', ()=> {
+            nav.classList.toggle('nav-active');
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+                }
+                
+            });
+    
+            burger.classList.toggle('toggle')
+        });
+    
+    
+    }
+    navSlide();
