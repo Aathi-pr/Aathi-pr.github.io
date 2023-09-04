098c811438e2f1ea6a76ef3063f743c5353f7a19
adithyan pr <108983785+Aathi-pr@users.noreply.github.com>
2023-09-05 01:45:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+
+function startTypingAnimation() {
+        const typedOutElement = document.querySelector('.typed-out');
+        
+        const newElement = typedOutElement.cloneNode(true);
+        typedOutElement.parentNode.replaceChild(newElement, typedOutElement);
+  
+        setTimeout(startTypingAnimation, 5000);
+      }
+
+      window.addEventListener('load', startTypingAnimation);
+
+const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.navigation');
+        const navLinks = document.querySelectorAll('.navigation li');
+    
+        burger.addEventListener('click', ()=> {
+            nav.classList.toggle('nav-active');
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+                }
+                
+            });
+    
+            burger.classList.toggle('toggle')
+        });
+    
+    
+    }
+    navSlide();
+
+
+
+    
